63c37d262ae6fd644353cb1febecda86c70b6e88
Gabriel Rodgers <gabrielrodgers4i@gmail.com>
2023-01-20 22:33:47 -0800

    Done with the unit test file

--- CS162-Assignment2/LemonadeStandTester.py
+++ CS162-Assignment2/LemonadeStandTester.py
@@ -14,7 +14,7 @@ class TestingLemonadeStand(unittest.TestCase):
 
     #create unit test methods (each method is a unit test)
     def test_1(self):
-        """Tests the MenuItem class"""
+        """Tests the MenuItem class get methods"""
         #create MenuItem classes for testing
         item1 = MenuItem("raspberries", 3, 6)
         item2 = MenuItem("blueberries", 2, 5)
@@ -33,7 +33,7 @@ class TestingLemonadeStand(unittest.TestCase):
         self.assertIsNot(lem.get_name(), "lalalaLa")
 
     def test_3(self):
-        """Tests the LemonadeStand """
+        """Tests the LemonadeStand menu_dict"""
 
         #create lemonadestand class
         lem = LemonadeStand("lalalala")
@@ -52,12 +52,96 @@ class TestingLemonadeStand(unittest.TestCase):
         self.assertEqual(6, lem._menu_dict.get("raspberries").get_selling_price())
 
     def test_4(self):
-        """Tests the """
+        """Tests the total_profit_for_menu_item method in the LemonadeStand class"""
+
+        # create lemonadestand class
+        lem = LemonadeStand("lalalala")
+
+        # create items to add to lem
+        item1 = MenuItem("raspberries", 3, 6)
+        item2 = MenuItem("blueberries", 2, 5)
+
+        # add items to lem menu_dict
+        lem.add_menu_item(item1)
+        lem.add_menu_item(item2)
+
+        #create sales_dict
+        day_x_sales = {
+            "raspberries" : 28,
+            "blueberries" : 87,
+        }
+
+        #add the sales dictionary to lem
+        lem.enter_sales_for_today(day_x_sales)
+
+        #test total_profit_for_menu_item method
+        self.assertEqual(84, lem.total_profit_for_menu_item("raspberries"))
+        self.assertAlmostEqual(261.00001, lem.total_profit_for_menu_item("blueberries"), 3)
 
     def test_5(self):
-        """"Tests the """
+        """"Testing the enter_sales_for_today method in the LemonadeStand class to raise the InvalidSaleItemError"""
+
+        # create lemonadestand class
+        lem = LemonadeStand("lalalala")
+
+        # create items to add to lem
+        item1 = MenuItem("raspberries", 3, 6)
+        item2 = MenuItem("blueberries", 2, 5)
+
+        # add items to lem menu_dict
+        lem.add_menu_item(item1)
+        lem.add_menu_item(item2)
+
+        # create sales_dict
+        day_x_sales = {
+            "raspberries": 28,
+            "blueberries": 87,
+            "rats" : -9
+        }
+
+        # add the sales dictionary to lem
+        try:
+            lem.enter_sales_for_today(day_x_sales)
+
+        except InvalidSalesItemError:
+            print("If you can see this message then this test successfully raised the InvalidSalesItemError")
 
     def test_6(self):
-        """Tests the """
+        """Testing the sales_dict inside the sales_record list in the enter_sales_for_today method"""
+
+        # create lemonadestand class
+        lem = LemonadeStand("lalalala")
+
+        # create items to add to lem
+        item1 = MenuItem("raspberries", 3, 6)
+        item2 = MenuItem("blueberries", 2, 5)
+        item3 = MenuItem("blue lemonade", 4, 25)
+
+        # add items to lem menu_dict
+        lem.add_menu_item(item1)
+        lem.add_menu_item(item2)
+        lem.add_menu_item(item3)
+
+        # create sales_dicts
+        day_0_sales = {
+            "raspberries": 28,
+            "blueberries": 87,
+        }
+
+        day_1_sales = {
+            "raspberries" : 76,
+            "blueberries" : 32,
+            "blue lemonade" : 12
+        }
+
+        #add sales_dicts to lem
+        lem.enter_sales_for_today(day_0_sales)
+        lem.enter_sales_for_today(day_1_sales)
+
+        #test the sales_record
+        self.assertIs(lem._sales_record[0].get_day(), 0)
+        self.assertIs(lem._sales_record[0].get_sales_dict().get("raspberries"), 28)
+
+
 
 
