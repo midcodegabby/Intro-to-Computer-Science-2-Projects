cfaa8feaeef299933c921dbd1a3813100da3b690
Gabriel Rodgers <gabrielrodgers4i@gmail.com>
2023-01-29 23:09:31 -0800

    ya

---
+++ CS162_Assignment4/.idea/bin_except.py
@@ -0,0 +1,45 @@
+#Author : Gabriel Rodgers
+#GitHub Username : trashcoder8
+#Date : 1/11/23
+#Description : This program is a modification of the basic binary search function that returns a TargetNotFound
+#exception instead of a -1 when the target value is not in the list.
+
+class TargetNotFound(Exception):
+    pass
+
+def binary_search(a_list, target):
+    """
+    Searches a_list for an occurrence of target.
+    If the target is found, returns the index of its position in the list
+    If the target is not found, raises the TargetNotFound exception, indicating that the target value isn't in the list
+    """
+    #define indices
+    first = 0
+    last = len(a_list) - 1
+
+    #iterates as long as the first index is lesser or equal to the last index
+    while first <= last:
+        #define midpoint index
+        middle = (first + last) // 2
+
+        #create if statement for the case where the midpoint index value is the target value
+        if a_list[middle] == target:
+            return middle
+
+        #create if statement for cases where the target is in the first half of the list
+        if a_list[middle] > target:
+            #updates last index so that while loop iterates over the first half of the list
+            last = middle - 1
+
+        #create else statement for cases where the target is in the second half of the list
+        else:
+            #updates first index so that the while loop iterates over the second half of the list
+            first = middle + 1
+
+    #only raised if the target is never found in the list
+    raise TargetNotFound
+
+list = [0, 4, 8, 13, 45, 83, 98, 293, 393, 585, 959, 1000]
+
+binary_search(list, 4)
+
