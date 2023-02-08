6427ea19007a1bc7bb8f8cac93b32b70fc4c02a1
Gabriel Rodgers <gabrielrodgers4i@gmail.com>
2023-02-07 18:10:05 -0800

    Time to test this assignment in GS

---
+++ CS162-Assignment5/NeighborhoodPets.py
@@ -0,0 +1,10 @@
+#Author : Gabriel Rodgers
+#GitHub Username : trashcoder8
+#Date : 2/6/23
+#Description :
+
+import json
+
+class NeighborhoodPets:
+    """This class simulates the pets in a neighborhood, and contains methods to add a pet, delete a pet, search for
+    the owner of a pet, save the data to a JSON file, load data from"""
