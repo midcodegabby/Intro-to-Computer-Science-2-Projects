779581543ce5bf5b777556ff186c5480c0ea0a6c
Gabriel Rodgers <gabrielrodgers4i@gmail.com>
2023-02-15 19:59:01 -0800

    Working on assignment 6d, have to program code to ensure repeat subproblems do not happen

--- CS162-Assignment6/row_puzzle.py
+++ CS162-Assignment6/row_puzzle.py
@@ -3,7 +3,7 @@
 #Date : 2/13/23
 #Description :
 
-def row_puzzle(a_list, token=0, memo=None):
+def row_puzzle(puz_list, token=0, puz_memo=None):
     """This recursive function takes as parameter a row (or list) of integers, with a zero at the highest index, and
     returns True if the puzzle is solvable for that row and False otherwise. The puzzle involves making the token,
     which starts at index 0, get to the last index of the list by moving to different index values (where the token can
@@ -11,46 +11,42 @@ def row_puzzle(a_list, token=0, memo=None):
     the row's bounds. This function does not change the contents of the row."""
 
     #create dictionary to store already indexed values
-    #if memo is None:
-        #memo = {}
+    if puz_memo is None:
+        puz_memo = {}
 
     #create base case
-    if token == len(a_list):
+    if token == len(puz_list):
         return True
 
-    #create base case for bound limitations
-    if token > len(a_list) or token < 0:
-        return None
+    #create movement variables
+    move_right = token + puz_list[token]
+    move_left = token - puz_list[token]
 
-    left = row_puzzle(a_list, -a_list[token])
-    right = row_puzzle(a_list, a_list[token])
+    #create if statement for case where a move to the right is in bounds
+    if move_right <= len(puz_list):
 
-    if
+        #update puzzle dict
+        puz_memo[token] = puz_list[token] #links a move to the right with a token's index value
 
+        return row_puzzle(puz_list, move_right, puz_memo)
 
+    #create elif statement for case where a move to the right is out of bounds
+    elif move_right > len(puz_list):
 
+        #create if statement for case where a move to the left is out of bounds
+        if move_left < 0:
+            return False #this is a dead end for a puzzle, must do something else
 
+        #create elif statement for case where a move to the left is in bounds
+        elif move_left >= 0:
 
-    if token not in memo:
-        memo[token] = a_list[token] #adds the token's index and value to the dictionary
+            #update puzzle dict
+            puz_memo[token] = -puz_list[token]  # links a move to the left with a token's index value
 
-        if a_list[token] < len(a_list):
-            return row_puzzle(a_list, a_list[token]-1,memo)
+            return row_puzzle(puz_list, move_left, puz_memo)
 
-    if token in memo:
-        token = a_list[token] - 1
-
-lst = [4, 3,1, 2, 2, 0]
-
-token = lst[0] - 1 #gives index
-print(token)
-
-token += lst[token]
-print(token)
-
-if token+1 == len(lst):
-    print("yes")
-
+list = [2, 4, 5, 3, 1, 3, 1, 4, 0]
+print(row_puzzle(list))
 
 
 
