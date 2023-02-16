cde10e0e5c2cb332abb94a6d4756c50961d2ee42
Gabriel Rodgers <gabrielrodgers4i@gmail.com>
2023-02-15 21:03:13 -0800

    done with assignment 6d...?

--- CS162-Assignment6/row_puzzle.py
+++ CS162-Assignment6/row_puzzle.py
@@ -1,53 +1,70 @@
 #Author : Gabriel Rodgers
 #GitHub Username : trashcoder8
-#Date : 2/13/23
-#Description :
+#Date : 2/15/23
+#Description : This program contains a recursive function row_puzzle that takes as parameter a list of integers with
+#a zero at the end, a default argument token that serves as the current index in the recursion and a default argument
+#puz_memo that is a dictionary of already visited indices in the list. The function's goal is to return True if the
+#token can be moved to the 0 at the end of the list by moving the token left or right by the same value as its current
+#index recursively multiple times, and False otherwise. This function also does not change the order or contents of
+#the list being solved.
 
 def row_puzzle(puz_list, token=0, puz_memo=None):
-    """This recursive function takes as parameter a row (or list) of integers, with a zero at the highest index, and
-    returns True if the puzzle is solvable for that row and False otherwise. The puzzle involves making the token,
-    which starts at index 0, get to the last index of the list by moving to different index values (where the token can
-    then move left or right the same number as the token's current indexed value), and the token cannot go out of
-    the row's bounds. This function does not change the contents of the row."""
+    """This recursive function takes as parameter a row (or list) of integers (puz_list), with a zero at the highest
+    index, and returns True if the puzzle is solvable for that row and False otherwise. The puzzle involves
+    making the token, which starts at index 0, get to the last index of the list by moving to different index values
+    (where the token can then move left or right the same number as the token's current indexed value),
+    and the token cannot go out of the row's bounds. This function does not change the contents of the row.
+    This function uses a dictionary puz_memo to hold indices in the list (keys) and their corresponding movements
+    from that index (values)."""
 
-    #create dictionary to store already indexed values
+    #initialize dictionary to store already indexed values
     if puz_memo is None:
         puz_memo = {}
 
     #create base case
-    if token == len(puz_list):
+    if token + 1 == len(puz_list):
         return True
 
     #create movement variables
     move_right = token + puz_list[token]
     move_left = token - puz_list[token]
 
-    #create if statement for case where a move to the right is in bounds
-    if move_right <= len(puz_list):
+    #create if statement for case where a move to the right is in bounds and the index has not yet been visited
+    if move_right < len(puz_list) and move_right not in puz_memo:
 
-        #update puzzle dict
-        puz_memo[token] = puz_list[token] #links a move to the right with a token's index value
+        ##links a move to the right with a token's index value
+        puz_memo[token] = puz_list[token]
 
-        return row_puzzle(puz_list, move_right, puz_memo)
+        return row_puzzle(puz_list, move_right, puz_memo) #recursive step
 
-    #create elif statement for case where a move to the right is out of bounds
-    elif move_right > len(puz_list):
+    #create elif statement for case where a move to the right is in bounds and the index has already been visited
+    #this will contain if statements that cause a move to the left instead of a move to the right for that index
+    elif move_right < len(puz_list) and move_right in puz_memo:
 
-        #create if statement for case where a move to the left is out of bounds
+        #create if statement for case where a move to the left is out of bounds (dead end, puzzle has no solution)
         if move_left < 0:
-            return False #this is a dead end for a puzzle, must do something else
+            return False
 
         #create elif statement for case where a move to the left is in bounds
         elif move_left >= 0:
 
-            #update puzzle dict
-            puz_memo[token] = -puz_list[token]  # links a move to the left with a token's index value
+            #link a move to the left with a token's index value in the puz_memo
+            puz_memo[token] = -puz_list[token]
 
-            return row_puzzle(puz_list, move_left, puz_memo)
+            return row_puzzle(puz_list, move_left, puz_memo) #recursive step
 
-list = [2, 4, 5, 3, 1, 3, 1, 4, 0]
-print(row_puzzle(list))
+    #create elif statement for case where a move to the right is out of bounds
+    elif move_right >= len(puz_list):
 
+        #create if statement for case where a move to the left is out of bounds (dead end, puzzle has no solution)
+        if move_left < 0:
+            return False
 
+        #create elif statement for case where a move to the left is in bounds
+        elif move_left >= 0:
 
+            #link a move to the left with a token's index value in the puz_memo
+            puz_memo[token] = -puz_list[token]
+
+            return row_puzzle(puz_list, move_left, puz_memo) #recursive step
 
